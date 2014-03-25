4ae887594f4250bfdc36b179cbada4217d38bbb7
Vladimir Levchuk <vladimir.levchuk@creuna.no>
2014-03-26 00:37:00 +0200

    - appveyor deployment script added
    Former-commit-id: 0444df02a1a405c280bdd98d7985fe3097b9ce1a

---
+++ deployment/Push-To-NuGet.ps1
@@ -0,0 +1,63 @@
+﻿Param (
+    $variables = @{},   
+    $artifacts = @{},
+    $scriptPath,
+    $buildFolder,
+    $srcFolder,
+    $outFolder,
+    $tempFolder,
+    $projectName,
+    $projectVersion,
+    $projectBuildNumber
+)
+
+Write-Output "Starting deployment: $projectName $projectVersion"
+
+# list all artifacts
+foreach($artifact in $artifacts.values)
+{
+    Write-Output "Artifact: $($artifact.name)"
+    Write-Output "Type: $($artifact.type)"
+    Write-Output "Path: $($artifact.path)"
+    Write-Output "Url: $($artifact.url)"
+}
+
+Write-Output "Defined variables:"
+
+$securedValue = "*************"
+# script custom variables
+foreach($name in $variables.keys)
+{
+    $value = $variables[$name]
+	$isSecured = $name.Contains("Secure") -or $name.ToLower().Contains("password")
+	if ($isSecured) {
+		$printedValue = $securedValue 
+	} else {
+		$printedValue = $value
+	}
+	
+    Write-Output "$name=$printedValue"
+}
+
+$nuget = "$srcFolder\.nuget\NuGet.exe"
+$apikey = $variables["apikeySecure"]
+$nugetPackage = $variables["nugetPackageName"]
+$nugetPackagesMask = "$tempFolder\$nugetPackage\*.nupkg"
+$packageFilename = $nugetPackagesMask
+$allPackages = Get-ChildItem $nugetPackagesMask
+
+foreach ($packageItem in $allPackages) 
+{
+	$packageFilename = $packageItem.ToString()
+	if (-not $packageFilename.Contains("symbols"))
+	{
+		$path = $packageFilename
+		break
+	}
+}
+
+$parameters = " Push ""$path"" -ApiKey $apikey"
+
+Write-Output "$nuget Push $path -ApiKey $securedValue"
+
+Invoke-Expression "$nuget $parameters"
